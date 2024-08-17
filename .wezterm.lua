a1c021d355c972a8179b3481e596f910d86f5f3a
fathyElgazar <gazzarDev@gmail.com>
2024-08-17 17:53:03 +0300

    track wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "Gruvbox Dark (Gogh)"
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 21
+config.default_cursor_style = "SteadyBar"
+config.enable_tab_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.7
+config.macos_window_background_blur = 10
+
+-- and finally, return the configuration to wezterm
+return config
