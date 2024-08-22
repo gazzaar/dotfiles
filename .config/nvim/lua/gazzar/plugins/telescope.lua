4a2ba3753817bb18775d9215c75158e55dfef586
gazzaar <gazzarDev@gmail.com>
2024-08-23 00:29:30 +0300

    edit trouble v3

--- .config/nvim/lua/gazzar/plugins/telescope.lua
+++ .config/nvim/lua/gazzar/plugins/telescope.lua
@@ -7,10 +7,20 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
-
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local transform_mod = require("telescope.actions.mt").transform_mod
+
+		local trouble = require("trouble")
+		local trouble_telescope = require("trouble.sources.telescope")
+
+		-- or create your custom action
+		local custom_actions = transform_mod({
+			open_trouble_qflist = function(prompt_bufnr)
+				trouble.toggle("quickfix")
+			end,
+		})
 
 		telescope.setup({
 			defaults = {
@@ -19,14 +29,15 @@ return {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+						["<C-t>"] = trouble_telescope.open,
 					},
 				},
 			},
 		})
 
 		telescope.load_extension("fzf")
-		telescope.load_extension("noice")
+
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
