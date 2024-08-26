bfcb293016a8f239b71c69ba848a9f01f65dda0d
gazzaar <gazzarDev@gmail.com>
2024-08-26 05:59:13 +0300

    add template and custom theme

--- .config/nvim/lua/gazzar/plugins/orgmode.lua
+++ .config/nvim/lua/gazzar/plugins/orgmode.lua
@@ -3,44 +3,43 @@ return {
 	event = "VeryLazy",
 	ft = { "org" },
 	config = function()
-		-- Setup orgmode
+		local gruvbox = require("gruvbox") -- Setup orgmode
 		require("orgmode").setup({
 			org_agenda_files = "~/.nb/klog/**/*",
 			org_default_notes_file = "~/.nb/klog/area/log.org",
+			org_todo_keywords = { "TODO(t)", "DONE", "CANCELED" },
+			org_todo_keyword_faces = {
+				TODO = ":foreground " .. gruvbox.palette.neutral_orange .. " :weight bold",
+				DONE = ":foreground " .. gruvbox.palette.neutral_blue .. " :weight bold",
+				CANCELED = ":foreground " .. gruvbox.palette.neutral_purple .. " :weight bold",
+			},
+			org_hide_emphasis_markers = true,
+			org_startup_indented = true,
+			org_adapt_indentation = true,
+
 			org_capture_templates = {
 				i = {
 					description = "Ideas",
-					template = "%?\n  %u\n\n",
+					template = "* %?\n  %u\n\n",
 					target = "~/.nb/klog/area/ideas.org",
-					properties = {
-						empty_lines = {
-							before = 1,
-							after = 1,
-						},
-					},
+					-- properties = {
+					-- 	empty_lines = {
+					-- 		before = 1,
+					-- 		after = 1,
+					-- 	},
+					-- },
 				},
 				l = {
 					description = "Quick Links",
-					template = "- [[%x][%?]]\n  %u\n",
+					template = "* [[%x][%?]]\n  %u\n",
 					target = "~/.nb/klog/resources/links.org",
-					properties = {
-						empty_lines = {
-							before = 1,
-							after = 1,
-						},
-					},
+					-- properties = {
+					-- 	empty_lines = {
+					-- 		before = 1,
+					-- 		after = 1,
+					-- 	},
+					-- },
 				},
-				-- m = {
-				-- 	description = "Multiple Todos",
-				-- 	template = "* TODO Math: %?\n  %t\n* TODO CS: %?\n  %t\n* TODO Web: %?\n  %t\n* TODO Quran: %?\n  %t\n* TODO Misc: %?\n  %t\n",
-				-- 	target = "~/.nb/klog/area/log.org",
-				-- 	properties = {
-				-- 		empty_lines = {
-				-- 			before = 1,
-				-- 			after = 1,
-				-- 		},
-				-- 	},
-				-- },
 			},
 		})
 	end,
