de830f59cbac3a265e03d934ed99a56bb1c7c171
gazzaar <gazzarDev@gmail.com>
2024-08-28 20:11:37 +0300

    change the color scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 -- config.color_scheme = "Gruvbox dark, soft (base16)"
 config.force_reverse_video_cursor = true
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "Tokyo Night Moon"
 -- config.font = wezterm.font("MesloLGS Nerd Font Mono")
 -- -- Fonts
 config.font_size = 21
@@ -37,7 +37,7 @@ config.window_padding = {
 	bottom = 0,
 }
 config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.9
+-- config.window_background_opacity = 0.7
 -- config.macos_window_background_blur = 10
 -- and finally, return the configuration to wezterm
 return config
