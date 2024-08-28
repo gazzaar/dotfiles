48a2495df11974ac3f8074caadb5c583c44d778d
gazzaar <gazzarDev@gmail.com>
2024-08-28 20:39:32 +0300

    add tokyo theme

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -10,15 +10,27 @@
 # export BACKGROUND_SPACE=0xffa6e3a1
 # export APP_NAME_COLOR=0xff11111b
 # export WHITE=0xffcdd6f4
+#
+# Tokyo Night Theme Color Palette
+export BAR_COLOR=0xff1a1b26        # Deep blue background
+export ITEM_BG_COLOR=0xff24283b    # Slightly lighter blue for right items
+export SEPARATOR_COLOR=0xff7aa2f7  # Bright blue for separators
+export BACKGROUND_SPACE=0xff24283b # Same as ITEM_BG_COLOR for consistency
+export ITEMS_COLOR=0xffff9e64      # Soft orange for item text
+export WHITE=0xffa9b1d6            # Light lavender for text under right items
 
-
+# Additional accent colors you might want to use:
+# export NEON_PINK=0xffff007c      # For highlights or special elements
+# export ELECTRIC_BLUE=0xff39d7ff  # For additional accents
+# export NEON_YELLOW=0xffe0af68    # For subtle highlights
+#
+###############################################
 # for gruvbox scheme
 
-export BAR_COLOR=0xff282828
-export ITEM_BG_COLOR=0xff4E4E4E # right items
-export SEPARATOR_COLOR=0xff83a598
-export BACKGROUND_SPACE=0xff4E4E4E
-export ITEMS_COLOR=0xffFFD7AF
-export WHITE=0xffA8A8A8 # text under right items
-#
-
+# export BAR_COLOR=0xff282828
+# export ITEM_BG_COLOR=0xff4E4E4E # right items
+# export SEPARATOR_COLOR=0xff83a598
+# export BACKGROUND_SPACE=0xff4E4E4E
+# export ITEMS_COLOR=0xffFFD7AF
+# export WHITE=0xffA8A8A8 # text under right items
+# #
