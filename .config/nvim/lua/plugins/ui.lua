33b31196f3ec2f91cdc5cdb086fc3e6a06c47510
gazzaar <gazzarDev@gmail.com>
2024-08-29 07:14:15 +0300

    adjust ui

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/noice.nvim",
+  opts = function(_, opts)
+    table.insert(opts.routes, {
+      filter = {
+        event = "notify",
+        find = "No informatin available",
+      },
+      opts = { skip = true },
+    })
+    opts.presets.lsp_doc_border = true
+  end,
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+  .--./)                                                               
+ /.''\\                                                       .-,.--.  
+| |  | |      __                              __        __    |  .-. | 
+ \`-' /    .:--.'.  .--------. .--------.  .:--.'.   .:--.'.  | |  | | 
+ /("'`    / |   \ | |____    | |____    | / |   \ | / |   \ | | |  | | 
+ \ '---.  `" __ | |     /   /      /   /  `" __ | | `" __ | | | |  '-  
+  /'""'.\  .'.''| |   .'   /     .'   /    .'.''| |  .'.''| | | |      
+ ||     ||/ /   | |_ /    /___  /    /___ / /   | |_/ /   | |_| |      
+ \'. __// \ \._,\ '/|         ||         |\ \._,\ '/\ \._,\ '/|_|      
+  `'---'   `--'  `" |_________||_________| `--'  `"  `--'  `"          
+
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
