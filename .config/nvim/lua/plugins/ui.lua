35bde04fa201da2d7e2f44554e2fce68578292ac
gazzaar <gazzarDev@gmail.com>
2024-08-29 09:46:00 +0300

    fix lua table

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,15 +1,18 @@
 return {
-  "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = {
-        event = "notify",
-        find = "No informatin available",
-      },
-      opts = { skip = true },
-    })
-    opts.presets.lsp_doc_border = true
-  end,
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
   {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
