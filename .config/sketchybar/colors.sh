f1bf58dc8d29dc14b4158d5673816169ea479ef3
gazzaar <gazzarDev@gmail.com>
2024-12-13 01:29:33 +0200

    remove some stuff

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -47,9 +47,10 @@
 
 export BAR_COLOR=0xff282828
 # export BAR_COLOR=0xC0282828 #transparency
-export ITEM_BG_COLOR=0xff4E4E4E # right items
+# export ITEM_BG_COLOR=0xff4E4E4E # right items
 export SEPARATOR_COLOR=0xff83a598
 export BACKGROUND_SPACE=0xff4E4E4E
 export ITEMS_COLOR=0xffFFD7AF
-export WHITE=0xffA8A8A8 # text under right items
+# export WHITE=0xffA8A8A8 # text under right items
+export WHITE=0xffd5c4a1
 # #
