1227b03590a9ae57b329e90dfaf941e4ee2bf747
gazzaar <gazzarDev@gmail.com>
2025-01-09 03:21:03 +0200

    clear initial colors

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,56 +1,9 @@
 #!/bin/bash
 
-# -- Teal Scheme --
-# 1e1e2e
-# for catpputin
-# export BAR_COLOR=0xff1e1e2e
-# export ITEM_BG_COLOR=0xff313244
-# export ACCENT_COLOR=0xff2cf9ed
-# export SEPARATOR_COLOR=0xffb4befe
-# export BACKGROUND_SPACE=0xffa6e3a1
-# export APP_NAME_COLOR=0xff11111b
-# export WHITE=0xffcdd6f4
-#
-# Tokyo Night Theme Color Palette
-# export BAR_COLOR=0xff1a1b26        # Deep blue background
-# export ITEM_BG_COLOR=0xff24283b    # Slightly lighter blue for right items
-# export SEPARATOR_COLOR=0xff7aa2f7  # Bright blue for separators
-# export BACKGROUND_SPACE=0xff24283b # Same as ITEM_BG_COLOR for consistency
-# export ITEMS_COLOR=0xffff9e64      # Soft orange for item text
-# export WHITE=0xffa9b1d6            # Light lavender for text under right items
-
-#############33 For  rosi pine################
-#Deep background - using base00 (dark base)
-# export BAR_COLOR=0xff232136
-#
-# # Slightly lighter background for items - using base01
-# export ITEM_BG_COLOR=0xff393552
-# # Accent color for separators - using base0D (purple)
-# export SEPARATOR_COLOR=0xffc4a7e7
-#
-# # Background space - matching ITEM_BG_COLOR for consistency
-# export BACKGROUND_SPACE=0xff393552
-#
-# # Item text color - using base09 (warm gold)
-# export ITEMS_COLOR=0xfff6c177
-#
-# # Secondary text color - using base05 (light text)
-# export WHITE=0xffe0def4
-# Additional accent colors you might want to use:
-# export NEON_PINK=0xffff007c      # For highlights or special elements
-# export ELECTRIC_BLUE=0xff39d7ff  # For additional accents
-# export NEON_YELLOW=0xffe0af68    # For subtle highlights
-#
-# export BACKGROUND_SPACE =0xff414868
-###############################################
-# for gruvbox scheme
-
 export BAR_COLOR=0xff282828
-# export BAR_COLOR=0xC0282828 #transparency
 # export ITEM_BG_COLOR=0xff4E4E4E # right items
 export SEPARATOR_COLOR=0xff83a598
 export BACKGROUND_SPACE=0xff4E4E4E
 export ITEMS_COLOR=0xffFFD7AF
 # export WHITE=0xffA8A8A8 # text under right items
 export WHITE=0xffd5c4a1
-# #
