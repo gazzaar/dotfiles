b825341714f5c23ad4a9bc0f3a9b84a274b4d10c
gazzaar <gazzarDev@gmail.com>
2025-10-12 23:03:28 +0300

    theme

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -7,3 +7,10 @@ export BACKGROUND_SPACE=0xff4E4E4E
 export ITEMS_COLOR=0xffFFD7AF
 # export WHITE=0xffA8A8A8 # text under right items
 export WHITE=0xffd5c4a1
+
+#### Rose-pine ####
+# export BAR_COLOR=0xff232136
+# export SEPARATOR_COLOR=0xff3e8fb0
+# export BACKGROUND_SPACE=0xff393552
+# export ITEMS_COLOR=0xfff6c177
+# export WHITE=0xffe0def4
