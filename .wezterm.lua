42f8c6f45bbe8b81a922f402a21ca93458bec796
gazzaar <gazzarDev@gmail.com>
2025-10-12 23:02:53 +0300

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,29 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
--- This will hold the configuration.
+
 local config = wezterm.config_builder()
--- This is where you actually apply your config choices
+
+-- Function to choose scheme based on system appearance
+local function choose_scheme()
+	local appearance = wezterm.gui.get_appearance()
+	if appearance:find("Dark") then
+		return "Gruvbox Dark (Gogh)"
+	else
+		return "Gruvbox Light"
+	end
+end
+
 config.force_reverse_video_cursor = true
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = choose_scheme()
+
 config.colors = {
-	foreground = "#d5c4a1",
+	-- foreground = "#d5c4a1",
 }
--- config.font = wezterm.font("MesloLGS Nerd Font Mono")
+
 config.max_fps = 120
--- -- Fonts
 config.font_size = 20
 config.font = wezterm.font({ family = "FiraCode Nerd Font" })
 config.bold_brightens_ansi_colors = true
+
 config.font_rules = {
 	{
 		intensity = "Bold",
@@ -31,17 +41,20 @@ config.font_rules = {
 		font = wezterm.font({ family = "Maple Mono", style = "Italic" }),
 	},
 }
+
 config.default_cursor_style = "SteadyBar"
 config.enable_tab_bar = false
+
 config.window_padding = {
 	left = 3,
 	right = 0,
 	top = 3,
 	bottom = 0,
 }
+
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 8
--- and finally, return the configuration to wezterm
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 9
+
 return config
