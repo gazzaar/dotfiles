180137e028fe6131080a34f428066c84a0db2e3e
gazzaar <gazzarDev@gmail.com>
2025-10-31 13:29:38 +0200

    add floating term

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,7 +111,10 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.conceallevel = 2
+vim.opt.guicursor = 'n-v-c:block,i:block'
 -- vim.opt.concealcursor = 'n'
+-- vim.o.background = 'light'
+vim.opt.colorcolumn = '80'
 
 -- Save undo history
 vim.o.undofile = true
@@ -165,6 +168,77 @@ vim.o.confirm = true
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- :Ntree
+vim.keymap.set('n', '<leader>e', function()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+    if bufname:match '^netrw' then
+      vim.api.nvim_win_close(win, true)
+      return
+    end
+  end
+  vim.cmd 'Vexplore'
+end, { desc = 'Toggle Netrw' })
+
+-- Floating terminal
+
+vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
+
+local state = {
+  floating = {
+    buf = -1,
+    win = -1,
+  },
+}
+
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+
+  -- Calculate the position to center the window
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+
+  -- Create a buffer
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+  end
+
+  -- Define window configuration
+  local win_config = {
+    relative = 'editor',
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = 'minimal', -- No borders or extra UI elements
+    border = 'rounded',
+  }
+
+  -- Create the floating window
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  return { buf = buf, win = win }
+end
+
+local toggle_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_floating_window { buf = state.floating.buf }
+    if vim.bo[state.floating.buf].buftype ~= 'terminal' then
+      vim.cmd.terminal()
+    end
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+  end
+end
+
+vim.api.nvim_create_user_command('Floaterminal', toggle_terminal, {})
+vim.keymap.set('n', '<space>tt', toggle_terminal, { desc = 'Toggle float terminal' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
@@ -211,6 +285,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
+  end,
+})
+
+-- Open the buffer at the last position
+vim.api.nvim_create_augroup('last_position', { clear = true })
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = 'last_position',
+  callback = function()
+    local mark_pos = vim.api.nvim_buf_get_mark(0, '"')
+    local last_line = vim.api.nvim_buf_line_count(0)
+    if mark_pos[1] > 0 and mark_pos[1] <= last_line then
+      vim.api.nvim_win_set_cursor(0, mark_pos)
+    end
   end,
 })
 
